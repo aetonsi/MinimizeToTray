7d416900a2bb92ba6d90c47d64d853a0c23e76bc
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-05-05 15:16:49 +0200

    v1.0.1

--- MinimizeToTray/Form1.Designer.cs
+++ MinimizeToTray/Form1.Designer.cs
@@ -31,7 +31,7 @@ namespace MinimizeToTray
         {
             this.components = new System.ComponentModel.Container();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -39,34 +39,39 @@ namespace MinimizeToTray
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
-            // listBox1
+            // textBox1
             // 
-            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(0, 0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(800, 450);
-            this.listBox1.TabIndex = 1;
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(800, 450);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.NotifyIcon notifyIcon1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- MinimizeToTray/Form1.cs
+++ MinimizeToTray/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
 
 namespace MinimizeToTray
 {
     public partial class Form1 : Form
     {
+        public Process process;
         public Form1()
         {
             InitializeComponent();
@@ -19,28 +22,77 @@ namespace MinimizeToTray
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            notifyIcon1.DoubleClick += (s, e) =>
+            BeginInvoke(new MethodInvoker(delegate
             {
-                Visible = !Visible;
-            };
+                Hide();
+            }));
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            
+
+            string[] args = Environment.GetCommandLineArgs();
+            string program = "cmd.exe";
+            string arguments = "/c " + string.Join(" ", args.Skip(1).ToArray()) + " 2>&1";
+            textBox1.AppendText(program + " " + arguments + System.Environment.NewLine);
+            textBox1.Text += System.Environment.NewLine + ("=================");
+
+
+            notifyIcon1.DoubleClick += (s, e) => Visible = !Visible;
             notifyIcon1.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
             notifyIcon1.Visible = true;
-            notifyIcon1.Text = Application.ProductName;
+            notifyIcon1.Text = Application.ProductName + 
+                " v" + Application.ProductVersion + 
+                " - " + Path.GetFileName(args.Skip(1).ToArray()[0]);
 
-            var contextMenu = new ContextMenuStrip();
-            contextMenu.Items.Add("Exit", null, (s, e) => { Application.Exit(); });
+
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add(program + " " + arguments, null, (s, e) => Clipboard.SetText(program + " " + arguments) );
+            contextMenu.Items.Add("---", null, (s, e) => { });
+            contextMenu.Items.Add("Show", null, (s, e) => this.Show());
+            contextMenu.Items.Add("Hide", null, (s, e) => this.Hide());
+            contextMenu.Items.Add("Exit", null, (s, e) => {
+                if(!process.HasExited)
+                {
+                    process.Kill(true);
+                }
+                this.Close();
+                Application.Exit();
+            });
             notifyIcon1.ContextMenuStrip = contextMenu;
+              
 
-            Console.WriteLine("Running!");
+            process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = program;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.ErrorDialog = false;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.OutputDataReceived += (sender, args) => textBox1.AppendText(System.Environment.NewLine + (args.Data));
+            process.Exited += (sender, args) =>
+            {
+                textBox1.AppendText(System.Environment.NewLine + " ========================================== ");
+                this.Show();
+                notifyIcon1.Visible = false;
+            };
 
-            // Standard message loop to catch click-events on notify icon
-            // Code after this method will be running only after Application.Exit()
-            string[] args = Environment.GetCommandLineArgs();
-            args = args.Skip(1).ToArray();
-            listBox1.Items.Add(string.Join(",", args));
 
-
-            notifyIcon1.Visible = false;
+            process.Start();
+            // process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
         }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            StreamWriter myStreamWriter = process.StandardInput;
+            myStreamWriter.Write(e.KeyChar);
+            e.Handled = true;
+            string text = e.KeyChar.ToString().Replace("\r", "\r\n");
+            textBox1.AppendText(text);
+        }
+
     }
 }
