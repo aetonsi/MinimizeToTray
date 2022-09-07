049dabe2a087e63fa22b0a6907815b39974d73ab
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-09-08 00:55:09 +0200

    cleanup command line variables, added clean comments

--- MinimizeToTray/Form1.cs
+++ MinimizeToTray/Form1.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿// *1 REMOVED BECAUSE IT IS IMPOSSIBLE TO MAINTAIN CONSOLE OUTPUT ORDER
+// *2 REMOVED BECAUSE IT MAKES IT IMPOSSIBLE TO TYPE INTO CONSOLE WINDOWS LAUNCHED BY THE REQUESTED PROGRAM
+
+using System;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using System.Diagnostics;
-using System.IO;
 
 namespace MinimizeToTray
 {
     public partial class Form1 : Form
     {
-        public string[] args;
+        public string[] arguments;
+        public string requestedProgramPath;
+        public string[] requestedProgramArguments;
+        public string internalProgramPath;
+        public string[] internalProgramArguments;
         public Icon icon;
         public ToolTipIcon balloonIcon;
         public Process process;
@@ -19,9 +25,20 @@ namespace MinimizeToTray
             InitializeComponent();
         }
 
-        private string productInfo()
+        private string requestedProgramCommandLine(bool alsorequestedProgramArguments = true)
         {
-            return Application.ProductName + " v" + Application.ProductVersion;
+            return requestedProgramPath + (alsorequestedProgramArguments ? " " + string.Join(" ", requestedProgramArguments) : "");
+        }
+
+        private string internalProgramCommandLine(bool alsoInternalProgramArgs = true)
+        {
+            return internalProgramPath + (alsoInternalProgramArgs ? " " + string.Join(" ", internalProgramArguments) : "");
+        }
+
+        private string productInfo(bool alsoRequestedProgramCommandLine = false)
+        {
+            return Application.ProductName + " v" + Application.ProductVersion
+                + (alsoRequestedProgramCommandLine ? " - " + requestedProgramCommandLine() : "");
         }
 
         private void quit(int code)
@@ -47,36 +64,34 @@ namespace MinimizeToTray
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ///////////////////////////////// SETUP VARIABLES /////////////////////////////////
-            args = Environment.GetCommandLineArgs().Skip(1).ToArray();
+            ///////////////////////////////// SETUP VARIABLES AND PRELIMINARY CHECKS /////////////////////////////////
+            arguments = Environment.GetCommandLineArgs().Skip(1).ToArray();
+            if (!arguments.Any()) quit(1);
+            requestedProgramPath = arguments[0];
+            requestedProgramArguments = arguments.Skip(1).ToArray();
             icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
             balloonIcon = System.Windows.Forms.ToolTipIcon.Info;
+            internalProgramPath = "cmd.exe";
+            internalProgramArguments = (new string[] { "/c"})
+                .Concat(requestedProgramCommandLine().Split(" "))
+                .Concat(new string[] { "2>&1" }) // REDIRECTING ERR TO OUT BECAUSE OF *1
+                .ToArray();
 
 
-            ///////////////////////////////// SETUP FORM, CHECK ARGUMENTS /////////////////////////////////
-            if (!args.Any())
-            {
-                quit(1);
-            }
+            ///////////////////////////////// SETUP FORM /////////////////////////////////
             TextBox.CheckForIllegalCrossThreadCalls = false;
             //BeginInvoke(new MethodInvoker(delegate{Hide();}));
-
-
-            ///////////////////////////////// SETUP VARIABLES /////////////////////////////////
-            string program = "cmd.exe";
-            string arguments = "/c " + string.Join(" ", args) + " 2>&1";
-            textBox1.AppendText(program + " " + arguments + System.Environment.NewLine);
-            separator();
+            this.Text = productInfo(true);
 
 
             ///////////////////////////////// SETUP NOTIFY ICON /////////////////////////////////
             notifyIcon1.Icon = icon;
-            notifyIcon1.Text = productInfo() + " - " + Path.GetFileName(args[0]);
+            notifyIcon1.Text = productInfo(true);
             notifyIcon1.BalloonTipIcon = balloonIcon;
             notifyIcon1.BalloonTipTitle = productInfo();
-            notifyIcon1.BalloonTipText = "Still running in the tray: " + string.Join(" ", args);
+            notifyIcon1.BalloonTipText = "Still running in the tray: " + requestedProgramCommandLine();
             ContextMenuStrip contextMenu = new ContextMenuStrip();
-            contextMenu.Items.Add(program + " " + arguments, null, (s, e) => Clipboard.SetText(program + " " + arguments) );
+            contextMenu.Items.Add(internalProgramCommandLine(), null, (s, e) => Clipboard.SetText(internalProgramCommandLine()));
             contextMenu.Items.Add("---", null, (s, e) => { });
             contextMenu.Items.Add("Show", null, (s, e) => this.Show());
             contextMenu.Items.Add("Hide", null, (s, e) => this.Hide());
@@ -91,33 +106,37 @@ namespace MinimizeToTray
             ///////////////////////////////// SETUP PROCESS /////////////////////////////////
             process = new Process();
             // Configure the process using the StartInfo properties.
-            process.StartInfo.FileName = program;
+            process.StartInfo.FileName = internalProgramPath;
+            process.StartInfo.Arguments = string.Join(" ", internalProgramArguments);
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.ErrorDialog = false;
-            process.StartInfo.Arguments = arguments;
             process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             process.StartInfo.CreateNoWindow = true;
             process.StartInfo.RedirectStandardOutput = true;
-            // process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardError = true;
+            // process.StartInfo.RedirectStandardError = true; // SEE *1
+            // process.StartInfo.RedirectStandardInput = true; // SEE *2
             process.StartInfo.UseShellExecute = false;
             process.OutputDataReceived += (sender, args) => textBox1.AppendText(System.Environment.NewLine + (args.Data));
-            process.ErrorDataReceived += (sender, args) => textBox1.AppendText(System.Environment.NewLine + (args.Data));
+            // process.ErrorDataReceived += (sender, args) => textBox1.AppendText(System.Environment.NewLine + (args.Data)); // SEE *1
             process.Exited += (sender, args) =>
             {
                 separator();
-                Visible = true;
+                this.Visible = true;
             };
 
 
-            ///////////////////////////////// START PROCESS /////////////////////////////////
+            ///////////////////////////////// START PROGRAM /////////////////////////////////
+            textBox1.AppendText(internalProgramCommandLine() + System.Environment.NewLine);
+            separator();
             process.Start();
-            process.BeginErrorReadLine();
             process.BeginOutputReadLine();
+            // process.BeginErrorReadLine(); // SEE *1
+            // process.WaitForExit(); // DO NOT USE: HANGS EVERYTHING
             timer1.Start();
         }
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            // SEE *2
             /*StreamWriter myStreamWriter = process.StandardInput;
             myStreamWriter.Write(e.KeyChar);
             e.Handled = true;
@@ -133,8 +152,8 @@ namespace MinimizeToTray
                 if (process != null && !process.HasExited)
                 {
                     this.Visible = false;
-                e.Cancel = true;
-                notifyIcon1.ShowBalloonTip(5000);
+                    e.Cancel = true;
+                    notifyIcon1.ShowBalloonTip(5000);
                 } // else exit
             }
         }
@@ -142,19 +161,19 @@ namespace MinimizeToTray
         private void notifyIcon1_DoubleClick(object sender, EventArgs e)
         {
             ///////////////////////////////// TOGGLE VISIBILITY /////////////////////////////////
-            Visible = !Visible;
+            this.Visible = !this.Visible;
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
             ///////////////////////////////// HIDE ON STARTUP /////////////////////////////////
-            Visible = false;
+            this.Visible = false;
         }
 
         private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
         {
             ///////////////////////////////// SHOW FORM ON BALLOONTIP CLICK /////////////////////////////////
-            Visible = true;
+            this.Visible = true;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
